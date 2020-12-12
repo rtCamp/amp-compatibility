2ca6bb9eb02d77750d6ddd3206c59c37ddbdcf2f
Dhaval Parekh <dmparekh007@gmail.com>
2020-12-12 23:41:32 +0530

    Add sample rest api route

---
+++ amp-compatibility-server.git/app/Controllers/Http/RestApiV1SController.ts
@@ -0,0 +1,28 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class RestApiV1SController {
+
+	/**
+	 * API endpoint callback.
+	 *
+	 * @method GET
+	 *
+	 * @return object Response data.
+	 */
+	public async index() {
+		return {status: 'ok'};
+	}
+
+	/**
+	 * API endpoint callback.
+	 *
+	 * @method POST
+	 *
+	 * @return object Response data.
+	 */
+	public async store() {
+
+		return {status: 'ok'};
+	}
+
+}
