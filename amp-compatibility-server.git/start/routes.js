7236fd83328e6d89792267457fff4866ca8c0c55
Dhaval Parekh <dmparekh007@gmail.com>
2020-12-16 14:13:38 +0530

    Add default web view

--- amp-compatibility-server.git/start/routes.js
+++ amp-compatibility-server.git/start/routes.js
@@ -16,9 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use( 'Route' );
 
-Route.get( '/', () => {
-	return { greeting: 'Hello world in JSON' };
-} );
+
+Route.on( '/' ).render( 'welcome' );
 
 Route.group( () => {
 	Route.get( 'amp-wp', 'RestApiV1Controller.index' );
