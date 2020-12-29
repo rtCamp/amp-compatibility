bbe5d2236938a2863db645c7e83a1cc78ba615f4
Dhaval Parekh <dmparekh007@gmail.com>
2020-12-29 13:32:31 +0530

    Update format of request amp data. (#16)
    * Fix #15: Skip stale amp_validated_urls
    
    * Add support of multisite environment. in setup.sh
    
    * Fix #13: Prevent removal of subdirectory path in case of subdir MU setup
    
    * update siteinfo request and remove site health node
    
    * Add function doc for plugin function
    
    * Update theme code
    
    * Fix #14: Add theme version in error source
    Change hash algorithem from md5 to sha256
    Format the error and source details

--- amp-wp-dummy-data-generator.git/amp-send-data.php
+++ amp-wp-dummy-data-generator.git/amp-send-data.php
@@ -52,27 +52,25 @@ if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 	}
 } );
 
+/**
+ * Class AMP_Prepare_Data
+ */
 class AMP_Prepare_Data {
 
+	/**
+	 * To get amp data to send it to compatibility server.
+	 *
+	 * @return array
+	 */
 	public static function get_data() {
 
 		$amp_urls = static::get_amp_urls();
 
-		$domain = strtolower( wp_parse_url( home_url(), PHP_URL_HOST ) );
-		$domain = str_replace( [ 'www.' ], [ '' ], $domain );
-
-		$amp_settings = get_option( 'amp-options' );
-		$amp_settings = ( ! empty( $amp_settings ) && is_array( $amp_settings ) ) ? $amp_settings : [];
-
 		$request_data = [
-			'site_url'                   => $domain,
+			'site_url'                   => static::get_home_url(),
 			'site_info'                  => static::get_site_info(),
-			'amp_settings'               => $amp_settings,
-			'site_health'                => static::get_site_health(),
 			'plugins'                    => static::get_plugin_info(),
-			'themes'                     => [
-				static::normalize_theme_info( wp_get_theme() ),
-			],
+			'themes'                     => static::get_theme_info(),
 			'errors'                     => array_values( static::get_errors() ),
 			'error_sources'              => array_values( $amp_urls['error_sources'] ),
 			'amp_validated_environments' => array_values( $amp_urls['amp_validated_environments'] ),
@@ -82,6 +80,11 @@ class AMP_Prepare_Data {
 		return $request_data;
 	}
 
+	/**
+	 * To get site info.
+	 *
+	 * @return array Site information.
+	 */
 	protected static function get_site_info() {
 
 		$wp_type = 'single';
@@ -90,109 +93,54 @@ class AMP_Prepare_Data {
 			$wp_type = ( defined( 'SUBDOMAIN_INSTALL' ) && SUBDOMAIN_INSTALL ) ? 'subdomain' : 'subdir';
 		}
 
-		return [
-			'site_url'      => wp_parse_url( home_url(), PHP_URL_HOST ),
-			'site_title'    => get_bloginfo( 'site_title' ),
-			'php_version'   => phpversion(),
-			'mysql_version' => '',
-			'wp_version'    => get_bloginfo( 'version' ),
-			'wp_type'       => $wp_type, // single, subdomain, subdir
-			'platform'      => '', // vip, vipgo
+		global $_wp_using_ext_object_cache;
+
+		$active_theme = wp_get_theme();
+		$active_theme = static::normalize_theme_info( $active_theme );
+
+		$amp_settings = get_option( 'amp-options' );
+		$amp_settings = ( ! empty( $amp_settings ) && is_array( $amp_settings ) ) ? $amp_settings : [];
+
+		$loopback_status = '';
+
+		if ( class_exists( 'Health_Check_Loopback' ) ) {
+			$loopback_status = Health_Check_Loopback::can_perform_loopback();
+			$loopback_status = ( ! empty( $loopback_status->status ) ) ? $loopback_status->status : '';
+		}
+
+		$site_info = [
+			'site_url'                     => static::get_home_url(),
+			'site_title'                   => get_bloginfo( 'site_title' ),
+			'php_version'                  => phpversion(),
+			'mysql_version'                => '',
+			'wp_version'                   => get_bloginfo( 'version' ),
+			'wp_language'                  => get_bloginfo( 'language' ),
+			'wp_https_status'              => is_ssl(),
+			'wp_multisite'                 => $wp_type,
+			'wp_active_theme'              => $active_theme,
+			'object_cache_status'          => ( ! empty( $_wp_using_ext_object_cache ) ) ? true : false,
+			'libxml_version'               => ( defined( 'LIBXML_VERSION' ) ) ? LIBXML_VERSION : '',
+			'is_defined_curl_multi'        => ( function_exists( 'curl_multi_init' ) ),
+			'stylesheet_transient_caching' => '',
+			'loopback_requests'            => $loopback_status,
+			'amp_mode'                     => ( ! empty( $amp_settings['theme_support'] ) ) ? $amp_settings['theme_support'] : '',
+			'amp_version'                  => ( ! empty( $amp_settings['version'] ) ) ? $amp_settings['version'] : '',
+			'amp_plugin_configured'        => ( ! empty( $amp_settings['plugin_configured'] ) ) ? true : false,
+			'amp_all_templates_supported'  => ( ! empty( $amp_settings['all_templates_supported'] ) ) ? true : false,
+			'amp_supported_post_types'     => ( ! empty( $amp_settings['supported_post_types'] ) && is_array( $amp_settings['supported_post_types'] ) ) ? $amp_settings['supported_post_types'] : [],
+			'amp_supported_templates'      => ( ! empty( $amp_settings['supported_templates'] ) && is_array( $amp_settings['supported_templates'] ) ) ? $amp_settings['supported_templates'] : [],
+			'amp_mobile_redirect'          => ( ! empty( $amp_settings['mobile_redirect'] ) ) ? true : false,
+			'amp_reader_theme'             => ( ! empty( $amp_settings['reader_theme'] ) ) ? $amp_settings['reader_theme'] : '',
 		];
+
+		return $site_info;
 	}
 
-	public static function get_site_health() {
-
-		if ( ! class_exists( 'Health_Check' ) ) {
-			return [];
-		}
-
-		$health_check_instance     = new Health_Check();
-		$health_check_js_variables = [
-			'site_status' => [
-				'direct' => [],
-				'async'  => [],
-				'issues' => [
-					'good'        => 0,
-					'recommended' => 0,
-					'critical'    => 0,
-				],
-			],
-		];
-
-		$issue_counts = get_transient( 'health-check-site-status-result' );
-
-		if ( false !== $issue_counts ) {
-			$issue_counts = json_decode( $issue_counts );
-
-			$health_check_js_variables['site_status']['issues'] = $issue_counts;
-		}
-
-		$tests = Health_Check_Site_Status::get_tests();
-
-		// Don't run https test on localhost
-		if ( 'localhost' === preg_replace( '|https?://|', '', get_site_url() ) ) {
-			unset( $tests['direct']['https_status'] );
-		}
-
-		foreach ( $tests['direct'] as $test ) {
-			if ( is_string( $test['test'] ) ) {
-				$test_function = sprintf(
-					'get_test_%s',
-					$test['test']
-				);
-
-				if ( method_exists( $health_check_instance, $test_function ) && is_callable( array(
-						$health_check_instance,
-						$test_function,
-					) ) ) {
-					/**
-					 * Filter the output of a finished Site Health test.
-					 *
-					 * @param array  $test_result {
-					 *                            An associated array of test result data.
-					 *
-					 * @param string $label       A label describing the test, and is used as a header in the output.
-					 * @param string $status      The status of the test, which can be a value of `good`, `recommended` or `critical`.
-					 * @param array  $badge       {
-					 *                            Tests are put into categories which have an associated badge shown, these can be modified and assigned here.
-					 *
-					 * @param string $label       The test label, for example `Performance`.
-					 * @param string $color       Default `blue`. A string representing a color to use for the label.
-					 *                            }
-					 * @param string $description A more descriptive explanation of what the test looks for, and why it is important for the end user.
-					 * @param string $actions     An action to direct the user to where they can resolve the issue, if one exists.
-					 * @param string $test        The name of the test being ran, used as a reference point.
-					 *                            }
-					 *
-					 * @since 5.3.0
-					 *
-					 */
-					$health_check_js_variables['site_status']['direct'][] = apply_filters( 'site_status_test_result', call_user_func( array(
-						$health_check_instance,
-						$test_function,
-					) ) );
-					continue;
-				}
-			}
-
-			if ( is_callable( $test['test'] ) ) {
-				$health_check_js_variables['site_status']['direct'][] = apply_filters( 'site_status_test_result', call_user_func( $test['test'] ) );
-			}
-		}
-
-		foreach ( $tests['async'] as $test ) {
-			if ( is_string( $test['test'] ) ) {
-				$health_check_js_variables['site_status']['async'][] = array(
-					'test'      => $test['test'],
-					'completed' => false,
-				);
-			}
-		}
-
-		return $health_check_js_variables;
-	}
-
+	/**
+	 * To get list of all plugin's information.
+	 *
+	 * @return array List of plugin detail.
+	 */
 	protected static function get_plugin_info() {
 
 		$all_plugins       = get_plugins();
@@ -203,6 +151,13 @@ class AMP_Prepare_Data {
 		return $plugin_info;
 	}
 
+	/**
+	 * To get plugin information by plugin file.
+	 *
+	 * @param string $plugin_file Plugin file.
+	 *
+	 * @return array Plugin detail.
+	 */
 	protected static function normalize_plugin_info( $plugin_file ) {
 
 		$absolute_plugin_file = WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin_file;
@@ -225,14 +180,32 @@ class AMP_Prepare_Data {
 			'author_url'        => $plugin_data['AuthorURI'],
 			'requires_wp'       => $plugin_data['RequiresWP'],
 			'requires_php'      => $plugin_data['RequiresPHP'],
-			'file'              => $plugin_file,
 			'is_active'         => is_plugin_active( $plugin_file ),
 			'is_network_active' => is_plugin_active_for_network( $plugin_file ),
-			'is_suppressed'     => in_array( $slug, $suppressed_plugin_list, true ) ? $suppressed_plugins[ $slug ]['last_version'] : "",
+			'is_suppressed'     => in_array( $slug, $suppressed_plugin_list, true ) ? $suppressed_plugins[ $slug ]['last_version'] : '',
 		];
 
 	}
 
+	/**
+	 * To get list of themes.
+	 *
+	 * @return array List of theme information.
+	 */
+	protected static function get_theme_info() {
+
+		return [
+			static::normalize_theme_info( wp_get_theme() ),
+		];
+	}
+
+	/**
+	 * To normalize theme information.
+	 *
+	 * @param \WP_Theme $theme_object Theme object.
+	 *
+	 * @return array Normalize theme information.
+	 */
 	protected static function normalize_theme_info( $theme_object ) {
 
 		if ( empty( $theme_object ) || ! is_a( $theme_object, 'WP_Theme' ) ) {
@@ -241,9 +214,14 @@ class AMP_Prepare_Data {
 
 		$active_theme      = wp_get_theme();
 		$active_theme_slug = '';
+		$parent_theme      = '';
 
 		if ( ! empty( $active_theme ) && is_a( $active_theme, 'WP_Theme' ) ) {
 			$active_theme_slug = $active_theme->get_stylesheet();
+		}
+
+		if ( ! empty( $theme_object->parent() ) && ! is_a( $theme_object->parent(), 'WP_Theme' ) ) {
+			$parent_theme = $theme_object->get_stylesheet();
 		}
 
 		$theme_data = [
@@ -258,16 +236,21 @@ class AMP_Prepare_Data {
 			'theme_url'    => $theme_object->get( 'ThemeURI' ),
 			'author'       => $theme_object->get( 'Author' ),
 			'author_url'   => $theme_object->get( 'AuthorURI' ),
-			'source'       => '', // wp_org, github, custom
 			'is_active'    => ( $theme_object->get_stylesheet() === $active_theme_slug ),
-			'parent_theme' => ( $theme_object->parent() ) ? self::normalize_theme_info( $theme_object->parent() ) : [],
+			'parent_theme' => $parent_theme,
 		];
 
 		return $theme_data;
 	}
 
+	/**
+	 * To get list of AMP errors.
+	 *
+	 * @return array List of errors.
+	 */
 	protected static function get_errors() {
 
+		$error_data      = [];
 		$amp_error_terms = get_terms(
 			[
 				'taxonomy'        => 'amp_validation_error',
@@ -276,27 +259,28 @@ class AMP_Prepare_Data {
 			]
 		);
 
-		$error_data = [];
-
 		foreach ( $amp_error_terms as $error_term ) {
 
 			if ( empty( $error_term ) || ! is_a( $error_term, 'WP_Term' ) ) {
 				continue;
 			}
 
-			$description  = strtolower( trim( $error_term->description ) );
-			$description  = static::remove_domain( $description );
+			// Remove site specific detail like site home_url() from error detail.
+			$description = strtolower( trim( $error_term->description ) );
+			$description = static::remove_domain( $description );
+
+			// Convert that into array.
 			$error_detail = json_decode( $description, true );
 
 			$error_detail['text'] = ( ! empty( $error_detail['text'] ) ) ? trim( $error_detail['text'] ) : '';
+			$error_detail['text'] = ( ! empty( $error_detail['text'] ) ) ? esc_html( $error_detail['text'] ) : '';
 
 			/**
 			 * Generate new slug after removing site specific data.
 			 */
-			$term_slug = static::generate_hash( $error_detail );
+			$error_detail['error_slug'] = static::generate_hash( $error_detail );
 
-			$error_detail['_slug'] = $term_slug;
-			$error_detail['text']  = ( ! empty( $error_detail['text'] ) ) ? esc_html( $error_detail['text'] ) : '';
+			ksort( $error_detail );
 
 			/**
 			 * Keep the slug as key to quickly get error detail.
@@ -312,6 +296,11 @@ class AMP_Prepare_Data {
 		return $error_data;
 	}
 
+	/**
+	 * To get amp validated URLs.
+	 *
+	 * @return array List amp validated URLs.
+	 */
 	protected static function get_amp_urls() {
 
 		global $wpdb;
@@ -328,13 +317,18 @@ class AMP_Prepare_Data {
 		// To store all AMP validated URls
 		$amp_invalid_urls = [];
 
-		$error_data  = static::get_errors();
-		$plugin_info = static::get_plugin_info();
-
+		$error_data      = static::get_errors();
+		$plugin_info     = static::get_plugin_info();
+		$theme_info      = static::get_theme_info();
 		$plugin_versions = [];
+		$theme_versions  = [];
 
 		foreach ( $plugin_info as $item ) {
 			$plugin_versions[ $item['slug'] ] = $item['version'];
+		}
+
+		foreach ( $theme_info as $item ) {
+			$theme_versions[ $item['slug'] ] = $item['version'];
 		}
 
 		/**
@@ -348,15 +342,26 @@ class AMP_Prepare_Data {
 				continue;
 			}
 
+			$staleness = AMP_Validated_URL_Post_Type::get_post_staleness( $amp_error_post->ID );
+
+			// Empty array for post staleness means post is NOT stale.
+			if ( ! empty( $staleness ) ) {
+				continue;
+			}
+
 			$post_errors_raw = json_decode( $amp_error_post->post_content, true );
 			$post_errors     = [];
+
+			if ( empty( $post_errors_raw ) ) {
+				continue;
+			}
 
 			/**
 			 * Process individual error in each post
 			 */
 			foreach ( $post_errors_raw as $error ) {
 
-				$_error_slug = $error_data[ $error['term_slug'] ]['_slug'];
+				$error_slug = $error_data[ $error['term_slug'] ]['error_slug'];
 
 				$sources            = ( ! empty( $error['data']['sources'] ) ) ? $error['data']['sources'] : [];
 				$post_error_sources = [];
@@ -367,33 +372,43 @@ class AMP_Prepare_Data {
 				foreach ( $sources as $index => $source ) {
 
 					if ( ! empty( $source['type'] ) ) {
+
 						if ( 'plugin' === $source['type'] ) {
 							$sources[ $index ]['version'] = $plugin_versions[ $source['name'] ];
 						} elseif ( 'theme' === $source['type'] ) {
-							$sources[ $index ]['version'] = '';
+							$sources[ $index ]['version'] = $theme_versions[ $source['name'] ];
 						}
 					}
 
-					if ( isset( $sources[ $index ]['text'] ) ) {
+					if ( ! empty( $sources[ $index ]['text'] ) ) {
 						$sources[ $index ]['text'] = strtolower( trim( $sources[ $index ]['text'] ) );
 						$sources[ $index ]['text'] = static::remove_domain( $sources[ $index ]['text'] );
 					}
 
-					$source_slug                      = self::generate_hash( $sources[ $index ] );
-					$sources[ $index ]['_slug']       = $source_slug;
-					$sources[ $index ]['_error_slug'] = $_error_slug;
-					$post_error_sources[]             = $source_slug;
+					// Generate error source slug.
+					$error_source_slug = self::generate_hash( $sources[ $index ] );
 
-					// Store in all source.
-					$all_sources[ $source_slug ] = $sources[ $index ];
-				}
+					// Update source information. Add error_slug and source_slug.
+					$sources[ $index ]['error_source_slug'] = $error_source_slug;
+					$sources[ $index ]['error_slug']        = $error_slug;
+
+					ksort( $sources[ $index ] );
+
+					// Store error source slug in current post list.
+					$post_error_sources[] = $error_source_slug;
+
+					// Store error source detail in all source list.
+					$all_sources[ $error_source_slug ] = $sources[ $index ];
+
+				} // Process on individual source complete.
 
 				$post_errors[] = [
-					'_error_slug' => $_error_slug,
-					'sources'     => array_values( $post_error_sources ),
+					'error_slug' => $error_slug,
+					'sources'    => array_values( $post_error_sources ),
 				];
-			}
+			} // Process on each post is completed.
 
+			// AMP Validated environment.
 			$amp_validated_environment          = get_post_meta( $amp_error_post->ID, '_amp_validated_environment', true );
 			$amp_validated_environment_slug     = static::generate_hash( $amp_validated_environment );
 			$amp_validated_environment['_slug'] = $amp_validated_environment_slug;
@@ -415,39 +430,75 @@ class AMP_Prepare_Data {
 		];
 	}
 
+	/**
+	 * To get home url of the site.
+	 * Note: It will give home url without protocol.
+	 *
+	 * @return string Home URL.
+	 */
+	protected static function get_home_url() {
+
+		$home_url = home_url();
+		$home_url = strtolower( trim( $home_url ) );
+
+		$http_protocol = wp_parse_url( $home_url, PHP_URL_SCHEME );
+
+		$home_url = str_replace( "$http_protocol://", '', $home_url );
+		$home_url = untrailingslashit( $home_url );
+
+		return $home_url;
+	}
+
+	/**
+	 * To remove home url from the content.
+	 *
+	 * @param string $content Content from home_url need to remove.
+	 *
+	 * @return string Content after removing home_url.
+	 */
 	protected static function remove_domain( $content ) {
 
 		if ( empty( $content ) ) {
 			return '';
 		}
 
-		$domain = strtolower( wp_parse_url( home_url(), PHP_URL_HOST ) );
-		$domain = str_replace( [ 'www.', '.' ], [ '', '\.' ], $domain );
+		$home_url = static::get_home_url();
+		$home_url = str_replace( [ '.', '/' ], [ '\.', '\\\\{1,5}\/' ], $home_url );
 
 		/**
-		 * Reference: https://regex101.com/r/JHc0Mt/1
+		 * Reference: https://regex101.com/r/c25pNF/1
 		 */
-		$regex = "/http[s]?:\\\\{0,5}\/\\\\{0,5}\/(www\.)?$domain/mU";
+		$regex = "/http[s]?:\\\\{0,5}\/\\\\{0,5}\/$home_url/mU";
 
 		$content = preg_replace( $regex, '', $content );
 
 		return $content;
 	}
 
+	/**
+	 * To generate hash of object.
+	 *
+	 * @param string|array|object $object Object for that hash need to generate.
+	 *
+	 * @return string Hash value of provided object.
+	 */
 	protected static function generate_hash( $object ) {
 
 		if ( empty( $object ) ) {
 			return '';
 		}
 
-		if ( is_string( $object ) ) {
-			$hash = md5( trim( $object ) );
-		} elseif ( is_array( $object ) ) {
-			ksort( $object );
-			$object = wp_json_encode( $object );
-			$hash   = md5( trim( $object ) );
+		if ( is_object( $object ) ) {
+			$object = (array) $object;
 		}
 
+		if ( is_array( $object ) ) {
+			ksort( $object );
+			$object = wp_json_encode( $object );
+		}
+
+		$object = trim( $object );
+		$hash   = hash( 'sha256', $object );
 
 		return $hash;
 	}
--- amp-wp-dummy-data-generator.git/setup.sh
+++ amp-wp-dummy-data-generator.git/setup.sh
@@ -10,36 +10,36 @@ setup_site() {
   wp rewrite flush
   wp cache flush
 
-  wp plugin install --activate wordpress-importer
-  wp plugin install --activate block-unit-test
-  wp plugin install --activate coblocks
+  wp plugin install --activate wordpress-importer $1
+  wp plugin install --activate block-unit-test $1
+  wp plugin install --activate coblocks $1
 
-  wp plugin install --activate amp
-  wp option update --json amp-options '{"theme_support":"standard"}'
+  wp plugin install --activate amp $1
+  wp option update --json amp-options '{"theme_support":"standard"}' $1
 
   # wp plugin install --activate amp-wp-dummy-data-generator
-  wp plugin activate amp-wp-dummy-data-generator
+  wp plugin activate amp-wp-dummy-data-generator $1
 }
 
 import_data() {
 
   ## Import data
-  import_files=$(wp amp-wp-dummy-data-generator get_import_files)
+  import_files=$(wp amp-wp-dummy-data-generator get_import_files $1)
 
   IFS='|' read -ra import_files_array <<< "$import_files"
 
   for import_file in "${import_files_array[@]}"; do
-    wp import --authors=create $plugin_dir/data/$import_file
+    wp import --authors=create $plugin_dir/data/$import_file $1
   done
 
   if [[ 0 == $(wp post list --post_type=attachment --post_name=accelerated-mobile-pages-is-now-just-amp --format=count) ]]; then
     wget https://blog.amp.dev/wp-content/uploads/2019/04/only_amp.mp4
-    wp media import --title="Accelerated Mobile Pages is now just AMP" only_amp.mp4
+    wp media import --title="Accelerated Mobile Pages is now just AMP" only_amp.mp4 $1
     rm only_amp.mp4
   fi
 
   ## Run Plugin CLI commands.
-  commands=$(wp amp-wp-dummy-data-generator get_plugin_commands)
+  commands=$(wp amp-wp-dummy-data-generator get_plugin_commands $1)
 
   IFS='|' read -ra command_array <<< "$commands"
 
@@ -48,23 +48,23 @@ import_data() {
   done
 
   ## Run After setup functions.
-  wp amp-wp-dummy-data-generator plugin_after_setup
+  wp amp-wp-dummy-data-generator plugin_after_setup $1
 
-  wp amp-wp-dummy-data-generator generate
+  wp amp-wp-dummy-data-generator generate $1
 
 }
 
 amp() {
 
   ## Make host entry
-  wp amp validation reset --yes
-  wp amp validation run --force
-  wp amp-send-data --print=json
+  wp amp validation reset --yes $1
+  wp amp validation run --force $1
+  wp amp-send-data --print=json $1
 
 }
 
-setup_site
+setup_site $1
 
-import_data
+import_data $1
 
-amp
+amp $1
