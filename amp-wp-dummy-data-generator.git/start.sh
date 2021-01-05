71ab589a08c1ff18bfcce0e5dfe709e4ff0bef0a
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-01-05 12:41:32 +0530

    Update wp-cli execution
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- amp-wp-dummy-data-generator.git/start.sh
+++ amp-wp-dummy-data-generator.git/start.sh
@@ -1,35 +1,40 @@
-# setup.sh
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -x
-set -e
+set -ex
 
 plugin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+[[ -n "$1" ]] && url_flag="$1" || url_flag=""
+
+function wp() {
+  wp_path=$(which wp)
+  php "$wp_path" "$url_flag" --allow-root "$@"
+}
 
 setup_site() {
   wp rewrite flush
   wp cache flush
 
-  wp plugin install --activate wordpress-importer $1
-  wp plugin install --activate block-unit-test $1
-  wp plugin install --activate coblocks $1
+  wp plugin install --activate wordpress-importer
+  wp plugin install --activate block-unit-test
+  wp plugin install --activate coblocks
 
-  wp plugin activate amp-wp-dummy-data-generator $1
+  wp plugin activate amp-wp-dummy-data-generator
 }
 
 import_data() {
 
   ## Import data from XML file.
-  import_files=$(wp amp-wp-dummy-data-generator get_import_files $1)
+  import_files=$(wp amp-wp-dummy-data-generator get_import_files)
 
   IFS='|' read -ra import_files_array <<< "$import_files"
 
   for import_file in "${import_files_array[@]}"; do
-    wp import --authors=create $plugin_dir/data/$import_file $1
+    wp import --authors=create $plugin_dir/data/$import_file
   done
 
   ## Run Plugin CLI commands.
-  commands=$(wp amp-wp-dummy-data-generator get_plugin_commands $1)
+  commands=$(wp amp-wp-dummy-data-generator get_plugin_commands)
 
   IFS='|' read -ra command_array <<< "$commands"
 
@@ -38,13 +43,13 @@ import_data() {
   done
 
   ## Run After setup functions.
-  wp amp-wp-dummy-data-generator plugin_after_setup $1
+  wp amp-wp-dummy-data-generator plugin_after_setup
 
-  wp amp-wp-dummy-data-generator generate $1
+  wp amp-wp-dummy-data-generator generate
 
 }
 
 
-setup_site $1
+setup_site
 
-import_data $1
+import_data
