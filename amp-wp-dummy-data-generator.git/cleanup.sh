5646ef7e334ca9da661f247522c80997833d41d9
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-01-19 12:39:52 +0530

    Remove exit on error and override wp
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- amp-wp-dummy-data-generator.git/cleanup.sh
+++ amp-wp-dummy-data-generator.git/cleanup.sh
@@ -1,12 +1,13 @@
-# setup.sh
-#!/bin/bash
-
-
+#!/usr/bin/env bash
 
 set -x
-set -e
 
 plugin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function wp() {
+  wp_path=$(which wp)
+  php "$wp_path" $url_flag --allow-root "$@"
+}
 
 wp rewrite flush
 wp cache flush
