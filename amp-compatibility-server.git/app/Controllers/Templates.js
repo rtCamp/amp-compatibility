3a8d55d70f55a1cfaae7e80d4cf1335464b461aa
Dhaval Parekh <dmparekh007@gmail.com>
2021-01-21 20:56:03 +0530

    Admin UI for listing queue's jobs detail (#69)
    * Add basic view in queue pages
    
    * WIP Add tabs in queue listing page
    
    * Update routes
    
    * Add Table to show job data of the queue in dashbaord
    
    * Change the post per page

---
+++ amp-compatibility-server.git/app/Controllers/Templates.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const View = use( 'View' );
+
+class Templates {
+
+	/**
+	 * Helper function to render pagination.
+	 *
+	 * @param {Object} options
+	 *
+	 * @return {string|*} HTML markup  for pagination.
+	 */
+	static renderPagination( options ) {
+
+		if ( ! options.total || ! options.perPage || ! options.baseUrl ) {
+			return '';
+		}
+
+		options.currentPage = parseInt( options.currentPage ) || 1;
+		options.totalPage = Math.ceil( options.total / options.perPage );
+
+		if ( 1 >= options.totalPage ) {
+			return '';
+		}
+
+		options.nextPage = ( options.currentPage < options.totalPage ) ? options.currentPage + 1 : false;
+		options.previousPage = ( 1 < options.currentPage ) ? options.currentPage - 1 : false;
+
+		options.showLimit = 5;
+		options.showPages = {
+			from: ( 1 <= options.currentPage - options.showLimit ) ? options.currentPage - options.showLimit : 1,
+			to: ( options.totalPage >= ( options.currentPage + options.showLimit ) ) ? options.currentPage + options.showLimit : options.totalPage,
+		}
+
+		options.showPreviousPadding = ( options.showLimit < options.currentPage - 1 );
+		options.showNextPadding = ( options.totalPage > ( options.currentPage + options.showLimit ) );
+
+		return View.render( 'templates/pagination', options );
+	}
+
+}
+
+module.exports = Templates
--- amp-compatibility-server.git/resources/js/dashboard.js
+++ amp-compatibility-server.git/resources/js/dashboard.js
@@ -1,4 +1,8 @@
 /* globals Chart:false, feather:false */
 import $ from 'jquery';
 window.$ = window.jQuery = $;
+'use strict';
+
+import $ from 'jquery';
+window.$ = window.jQuery = $;
 import bootstrap from 'bootstrap';
