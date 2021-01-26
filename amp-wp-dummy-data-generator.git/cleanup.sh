339973271f13103005c6bb2d6cb24b9ee6091b0c
Dhaval Parekh <dmparekh007@gmail.com>
2021-01-26 15:47:07 +0530

    Update formating for bash scripts

--- amp-wp-dummy-data-generator.git/cleanup.sh
+++ amp-wp-dummy-data-generator.git/cleanup.sh
@@ -5,8 +5,8 @@ set -x
 plugin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function wp() {
-  wp_path=$(which wp)
-  php "$wp_path" $url_flag --allow-root "$@"
+	wp_path=$(which wp)
+	php "$wp_path" $url_flag --allow-root "$@"
 }
 
 wp rewrite flush
@@ -15,14 +15,13 @@ wp cache flush
 #wp comment delete $(wp comment list --format=ids) --force
 #wp menu delete $(wp menu list --format=ids)
 
-
 post_types=$(wp post-type list --field=name | xargs echo $1)
 
 IFS=' ' # space is set as delimiter
 read -ra post_type_array <<< "$post_types" # str is read into an array as tokens separated by IFS
 
 for post_type in "${post_type_array[@]}"; do # access each element of array
-    wp post delete $(wp post list --post_type=$post_type --format=ids) --force
+	wp post delete $(wp post list --post_type=$post_type --format=ids) --force
 done
 
 taxonomies=$(wp taxonomy list --field=name | xargs echo $1)
@@ -31,7 +30,7 @@ IFS=' ' # space is set as delimiter
 read -ra taxonomy_array <<< "$taxonomies" # str is read into an array as tokens separated by IFS
 
 for taxonomy in "${taxonomy_array[@]}"; do # access each element of array
-    wp term list $taxonomy --field=term_id | xargs wp term delete $taxonomy
+	wp term list $taxonomy --field=term_id | xargs wp term delete $taxonomy
 done
 
 wp widget reset --all
@@ -41,4 +40,3 @@ wp transient delete --all
 upload_dir=$(wp eval 'echo wp_get_upload_dir()["basedir"];')
 rm -rf "$upload_dir"
 mkdir "$upload_dir"
-
--- amp-wp-dummy-data-generator.git/start.sh
+++ amp-wp-dummy-data-generator.git/start.sh
@@ -2,8 +2,6 @@
 
 plugin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# [[ -n "$1" ]] && url_flag="$1" || url_flag=""
-
 exclude_default_flag=""
 
 for i in "$@"; do
@@ -25,67 +23,67 @@ done
 
 
 function wp() {
-  wp_path=$(which wp)
-  php "$wp_path" $url_flag --allow-root "$@"
+	wp_path=$(which wp)
+	php "$wp_path" $url_flag --allow-root "$@"
 }
 
 setup_site() {
-  wp rewrite flush
-  wp cache flush
+	wp rewrite flush
+	wp cache flush
 
-  wp plugin install --activate wordpress-importer
-  wp plugin install --activate block-unit-test
-  wp plugin install --activate coblocks
+	wp plugin install --activate wordpress-importer
+	wp plugin install --activate block-unit-test
+	wp plugin install --activate coblocks
 
-  wp plugin activate amp-wp-dummy-data-generator
+	wp plugin activate amp-wp-dummy-data-generator
 }
 
 ### Actions before importing data.
 actions_before_importing() {
 
-  # CLI commands.
-  commands=$(wp amp-wp-dummy-data-generator get_commands --type=before "$exclude_default_flag")
-  IFS='|' read -ra command_array <<< "$commands"
+	# CLI commands.
+	commands=$(wp amp-wp-dummy-data-generator get_commands --type=before $exclude_default_flag)
+	IFS='|' read -ra command_array <<< "$commands"
 
-  for command in "${command_array[@]}"; do
-    $command
-  done
+	for command in "${command_array[@]}"; do
+		$command
+	done
 
-  # Custom actions.
-  wp amp-wp-dummy-data-generator run_custom --type=before "$exclude_default_flag"
+	# Custom actions.
+	wp amp-wp-dummy-data-generator run_custom --type=before $exclude_default_flag
 
 }
 
 ### Import Data.
 import_data() {
 
-  # Import data from XML file.
-  import_files=$(wp amp-wp-dummy-data-generator get_import_files "$exclude_default_flag")
+	# Import data from XML file.
+	import_files=$(wp amp-wp-dummy-data-generator get_import_files $exclude_default_flag)
 
-  IFS='|' read -ra import_files_array <<< "$import_files"
+	IFS='|' read -ra import_files_array <<< "$import_files"
 
-  for import_file in "${import_files_array[@]}"; do
-    wp import --authors=create $plugin_dir/data/$import_file
-  done
+	for import_file in "${import_files_array[@]}"; do
+		wp import --authors=create $plugin_dir/data/$import_file
+	done
 
-  # Generate custom content.
-  wp amp-wp-dummy-data-generator generate "$exclude_default_flag"
+	# Generate custom content.
+	wp amp-wp-dummy-data-generator generate $exclude_default_flag
 
 }
 
 ### Actions before importing data.
 actions_after_importing() {
 
-  # CLI commands
-  commands=$(wp amp-wp-dummy-data-generator get_commands --type=after "$exclude_default_flag")
-  IFS='|' read -ra command_array <<< "$commands"
+	# CLI commands
+	commands=$(wp amp-wp-dummy-data-generator get_commands --type=after $exclude_default_flag)
+	IFS='|' read -ra command_array <<< "$commands"
 
-  for command in "${command_array[@]}"; do
-    $command
-  done
+	for command in "${command_array[@]}"; do
+	$command
+	done
 
-  # Custom actions.
-  wp amp-wp-dummy-data-generator run_custom --type=after "$exclude_default_flag"
+	# Custom actions.
+	wp amp-wp-dummy-data-generator run_custom --type=after $exclude_default_flag
 
 }
 
