072cf5312c5953f892a395ffbaa91387a9009233
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-02-01 12:41:58 +0530

    Add steps to configure newrelic logging
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- amp-compatibility-server/scripts/setup-server.sh
+++ amp-compatibility-server/scripts/setup-server.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ -f ~/.bash_aliases ]]; then
+	. ~/.bash_aliases
+fi
 
 log_info1() {
 
@@ -133,7 +135,33 @@ function move_dummy_data_repo() {
 }
 
 function setup_base_data() {
-	bash "$base_dir/sites/base-site.sh"
+	bash "$HOME/amp-compatibility-server/sites/base-site.sh"
+}
+
+function create_log_dirs() {
+
+	mkdir -p /var/log/adonis
+	mkdir -p /var/log/sites
+}
+
+function setup_newrelic_infra_agent() {
+
+	curl -s https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | apt-key add -
+	echo "license_key: $NEWRELIC_LICENSE" > /etc/newrelic-infra.yml
+	printf "deb [arch=amd64] https://download.newrelic.com/infrastructure_agent/linux/apt buster main" | tee -a /etc/apt/sources.list.d/newrelic-infra.list
+	apt update
+	apt install newrelic-infra -y
+
+cat <<EOF > /etc/newrelic-infra/logging.d/amp-comp.log
+logs:
+  - name: "synthetic-sites-data"
+	file: /var/log/sites/*.log
+  - name: "adonis-logs"
+	file: /var/log/adonis/*.log
+  - name: "setup-logs"
+	file: /var/log/init.log
+EOF
+	systemctl restart newrelic-infra.service
 }
 
 function main() {
@@ -146,6 +174,8 @@ function main() {
 	setup_local_repo "amp-compatibility-server"
 	move_dummy_data_repo
 	setup_base_data
+	create_log_dirs
+	setup_newrelic_infra_agent
 }
 
 main
