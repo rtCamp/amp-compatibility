cfdd83a7c081f0e93aa12433ea4d383977cfc293
Dhaval Parekh <dmparekh007@gmail.com>
2021-02-22 13:31:39 +0530

    Add default shortcode on shortcode page

--- amp-wp-dummy-data-generator/inc/classes/class-shortcodes.php
+++ amp-wp-dummy-data-generator/inc/classes/class-shortcodes.php
@@ -52,12 +52,118 @@ class Shortcodes {
 		}
 
 		foreach ( $shortcode_tags as $shortcode_tag => $callback ) {
-			$shortcode_content = "<div><h3>$shortcode_tag</h3> [$shortcode_tag] </div>";
+			$shortcode_content = "<div><h3>$shortcode_tag</h3> " . $this->get_shortcode_markup( $shortcode_tag ) . " </div>";
 
 			$content .= '<br/>' . $shortcode_content;
 		}
 
 		return $content;
+	}
+
+
+	/**
+	 * To get short code markup.
+	 *
+	 * @param string $shortcode_tag Shortcode name.
+	 *
+	 * @return string Shortcode mark with args.
+	 */
+	public function get_shortcode_markup( $shortcode_tag ) {
+
+		if ( empty( $shortcode_tag ) ) {
+			return '';
+		}
+
+		$markup = '';
+
+		switch ( $shortcode_tag ) {
+			case 'wp_caption':
+			case 'caption':
+				$args          = [
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'image',
+					'posts_per_page' => 1,
+					'fields'         => 'ids',
+				];
+				$attachment_id = get_posts( $args );
+				$attachment_id = ( ! empty( $attachment_id[0] ) && 0 < intval( $attachment_id[0] ) ) ? intval( $attachment_id[0] ) : 0;
+
+				if ( $attachment_id ) {
+					$attachment_image = wp_get_attachment_image( $attachment_id, 'full' );
+					$markup           = "[$shortcode_tag  id='$shortcode_tag' align='alignleft']$attachment_image The Caption[/$shortcode_tag]";
+				}
+				break;
+			case 'gallery':
+				$args           = [
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'image',
+					'posts_per_page' => 9,
+					'fields'         => 'ids',
+				];
+				$attachment_ids = get_posts( $args );
+				$attachment_ids = ( ! empty( $attachment_ids ) && is_array( $attachment_ids ) ) ? $attachment_ids : [];
+
+				if ( ! empty( $attachment_ids ) ) {
+					$attachment_ids = implode( ',', $attachment_ids );
+					$markup         = "[$shortcode_tag ids='$attachment_ids']";
+				}
+				break;
+
+
+			case 'playlist':
+
+				$args           = [
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'audio',
+					'posts_per_page' => 9,
+					'fields'         => 'ids',
+				];
+				$attachment_ids = get_posts( $args );
+				$attachment_ids = ( ! empty( $attachment_ids ) && is_array( $attachment_ids ) ) ? $attachment_ids : [];
+
+				if ( ! empty( $attachment_ids ) ) {
+					$attachment_ids = implode( ',', $attachment_ids );
+					$markup         = "[$shortcode_tag ids='$attachment_ids']";
+				}
+				break;
+			case 'audio':
+				$args       = [
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'audio',
+					'posts_per_page' => 1,
+				];
+				$attachment = get_posts( $args );
+
+				if ( ! empty( $attachment[0] ) && is_a( $attachment[0], 'WP_Post' ) ) {
+					$src    = $attachment[0]->guid;
+					$markup = "[$shortcode_tag src='$src']";
+				}
+
+				break;
+			case 'video':
+				$args       = [
+					'post_type'      => 'attachment',
+					'post_mime_type' => 'video',
+					'posts_per_page' => 1,
+				];
+				$attachment = get_posts( $args );
+
+				if ( ! empty( $attachment[0] ) && is_a( $attachment[0], 'WP_Post' ) ) {
+					$src    = $attachment[0]->guid;
+					$markup = "[$shortcode_tag src='$src']";
+				}
+
+				break;
+			case 'embed':
+				$markup = "[$shortcode_tag src='https://youtu.be/zEmtfA8FETc']";
+				break;
+			default:
+				$markup = "[$shortcode_tag]";
+				break;
+		}
+
+		return $markup;
+
 	}
 
 }
