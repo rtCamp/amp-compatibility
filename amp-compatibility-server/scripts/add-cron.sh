857772ef11c8b7caf7eadff8f93578af2d8f512e
Riddhesh Sanghvi <riddhesh237@gmail.com>
2021-03-08 19:26:51 +0530

    Remove HOME and add cd instead
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- amp-compatibility-server/scripts/add-cron.sh
+++ amp-compatibility-server/scripts/add-cron.sh
@@ -5,20 +5,21 @@
 # Execution: bash add-cron.sh
 
 REPO_ROOT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )/../../" >/dev/null 2>&1 && pwd )"
+CD_PATH="$REPO_ROOT/amp-compatibility-server"
 
 CRONTAB_CONFIG=(
 	# Path of the application to update working directory for cron execution.
-	"HOME=$REPO_ROOT/amp-compatibility-server"
+	"PATH=$PATH"
 	"PM2_HOME=/root/.pm2"
 )
 
 CRONS=(
 	# Run every 2mins
-	"*/2 * * * * bash scripts/pull-cd.sh  > logs/cron/pull-cd.log 2>&1"
+	"*/2 * * * * cd "$CD_PATH" && bash scripts/pull-cd.sh  > logs/cron/pull-cd.log 2>&1"
 	# Run every day at 12AM
-	"0 0 * * * node ace cache:update && node ace wporg:scraper --use-stream --browse=updated > logs/cron/scrapper.log 2>&1 && node ace extension:mapping:update"
+	"0 0 * * * cd "$CD_PATH" && node ace cache:update && node ace wporg:scraper --use-stream --browse=updated > logs/cron/scrapper.log 2>&1 && node ace extension:mapping:update"
 	# Run at 4AM every Saturday
-	"0 4 * * 6 node ace synthetic-data:start --plugin-active-install=10000 --theme-active-install=1000 --number-of-instance=9 --concurrency=25 > logs/cron/synthetic-data-cron.log 2>&1"
+	"0 4 * * 6 cd "$CD_PATH" && node ace synthetic-data:start --plugin-active-install=10000 --theme-active-install=1000 --number-of-instance=9 --concurrency=25 > logs/cron/synthetic-data-cron.log 2>&1"
 )
 
 # Do not edit below this line.
