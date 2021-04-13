33aebfe10f34d3ff60c56bf254f5d029b16ee424
Dhaval Parekh <dmparekh007@gmail.com>
2021-04-13 12:32:49 +0530

    Add login with Google. (#124)
    * Add login with google
    
    * Update command to create user
    
    * Add CLI command to delete user
    
    * Add error message on failed attampt of login
    
    * Add const name for google login in env example file

--- amp-compatibility-server/app/Commands/UserRemove.js
+++ amp-compatibility-server/app/Commands/UserRemove.js
@@ -2,16 +2,15 @@
 
 const { Command } = require( '@adonisjs/ace' );
 const User = use( 'App/Models/User' );
-const Utility = use( 'App/Helpers/Utility' );
 const { exit } = require( 'process' );
 
-class UserCreate extends Command {
+class UserRemove extends Command {
 
 	/**
 	 * Command signature.
 	 */
 	static get signature() {
-		return `user:create
+		return `user:remove
 		 { --email=@value : Email address of user. }`;
 	}
 
@@ -21,12 +20,12 @@ class UserCreate extends Command {
 	 * @return {string} command description.
 	 */
 	static get description() {
-		return 'To create user for application';
+		return 'To remove user for application';
 	}
 
 	/**
 	 * To handle functionality of command.
-	 * To create user for dashboard.
+	 * To remove user for dashboard.
 	 *
 	 * @param {Object} args Argument passed in command.
 	 * @param {Object} options Options passed in command.
@@ -35,20 +34,12 @@ class UserCreate extends Command {
 	 */
 	async handle( args, options ) {
 
-		const email = options.email;
-		const username = email.substring( 0, email.indexOf( '@' ) );
-
-		const userData = {
-			username: username,
-			email: email,
-			password: `${ username }-${ Utility.random( 10000, 10000 ) }`,
-		};
-
 		try {
-			await User.create( userData );
-			this.success( 'User created.' );
+			const user = await User.findBy( 'email', options.email );
+			await user.delete();
+			this.success( 'User removed' );
 		} catch ( error ) {
-			this.error( 'Fail to create user.' );
+			this.error( 'Fail to remove user.' );
 		}
 
 		exit( 1 );
@@ -56,4 +47,4 @@ class UserCreate extends Command {
 	}
 }
 
-module.exports = UserCreate;
+module.exports = UserRemove;
