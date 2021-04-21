e2021c7b9470abae3d56ccafd78377d28dc24789
Dhaval Parekh <dmparekh007@gmail.com>
2021-04-21 14:05:15 +0530

    Update configuration for throttle by redis

--- amp-compatibility-server/config/throttle.js
+++ amp-compatibility-server/config/throttle.js
@@ -1,34 +1,34 @@
-'use strict'
+'use strict';
 
-const Env = use('Env')
+const Env = use( 'Env' );
 
 module.exports = {
-  /*
-  |--------------------------------------------------------------------------
-  | Throttle Driver
-  |--------------------------------------------------------------------------
-  |
-  | The throttle driver to be used for selecting throttle cache driver. It
-  | can be memory or redis.
-  |
-  | For `redis` driver, make sure to install and register `@adonisjs/redis`
-  |
-  */
-  driver: Env.get('THROTTLE_DRIVER', 'memory'),
+	/*
+	|--------------------------------------------------------------------------
+	| Throttle Driver
+	|--------------------------------------------------------------------------
+	|
+	| The throttle driver to be used for selecting throttle cache driver. It
+	| can be memory or redis.
+	|
+	| For `redis` driver, make sure to install and register `@adonisjs/redis`
+	|
+	*/
+	driver: Env.get( 'THROTTLE_DRIVER', 'memory' ),
 
-  /*
-  |--------------------------------------------------------------------------
-  | Redis config
-  |--------------------------------------------------------------------------
-  |
-  | The configuration for the redis driver.
-  |
-  */
-  redis: {
-    host: '127.0.0.1',
-    port: 6379,
-    password: null,
-    db: 0,
-    keyPrefix: ''
-  }
-}
+	/*
+	|--------------------------------------------------------------------------
+	| Redis config
+	|--------------------------------------------------------------------------
+	|
+	| The configuration for the redis driver.
+	|
+	*/
+	redis: {
+		host: Env.get( 'QUEUE_REDIS_HOST', '127.0.0.1' ),
+		port: Env.get( 'QUEUE_REDIS_PORT', 6379 ),
+		password: Env.get( 'QUEUE_REDIS_PASSWORD', null ),
+		db: 0,
+		keyPrefix: Env.get( 'REDIS_KEYPREFIX', 'local' ),
+	},
+};
