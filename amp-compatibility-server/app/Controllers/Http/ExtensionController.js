98089e53e07d75d075300898ab187dd59e2edf1a
Dhaval Parekh <dmparekh007@gmail.com>
2021-05-14 15:46:09 +0530

    Change extension per page for extension report

--- amp-compatibility-server/app/Controllers/Http/ExtensionController.js
+++ amp-compatibility-server/app/Controllers/Http/ExtensionController.js
@@ -36,7 +36,7 @@ class ExtensionController {
 	async index( { request, response, view, params } ) {
 		params = _.defaults( params, {
 			paged: 1,
-			perPage: 50,
+			perPage: 20,
 			s: request.input( 's' ) || '',
 			searchFields: [
 				'name',
