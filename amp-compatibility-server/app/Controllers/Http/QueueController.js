a4ebcc0392b43a7ea2072a18973d38c9f0baae83
Dhaval Parekh <dmparekh007@gmail.com>
2021-05-19 12:54:48 +0530

    Show queue data from MySQL instead of redis

--- amp-compatibility-server/app/Controllers/Http/QueueController.js
+++ amp-compatibility-server/app/Controllers/Http/QueueController.js
@@ -9,6 +9,8 @@ const RequestQueueController = use( 'App/Controllers/Queue/RequestController' );
 const SyntheticDataQueueController = use( 'App/Controllers/Queue/SyntheticDataController' );
 const AdhocSyntheticDataQueueController = use( 'App/Controllers/Queue/AdhocSyntheticDataController' );
 
+const Database = use( 'Database' );
+
 const { validateAll } = use( 'Validator' );
 const Utility = use( 'App/Helpers/Utility' );
 const _ = require( 'underscore' );
@@ -159,6 +161,252 @@ class QueueController {
 		};
 
 		return view.render( 'dashboard/queue-table', data );
+	}
+
+	/**
+	 * To render queue job listing table.
+	 *
+	 * @param {object} ctx
+	 * @param {View} ctx.view
+	 * @param {Object} ctx.params
+	 *
+	 * @return {Promise<*>}
+	 */
+	async indexMySQL( { view, params } ) {
+
+		params = _.defaults( params, {
+			queue: 'request-queue',
+			status: 'active',
+			paged: 1,
+			perPage: 50,
+		} );
+
+		const queueControllerList = {
+			'request-queue': RequestQueueController,
+			'synthetic-queue': SyntheticDataQueueController,
+			'adhoc-synthetic-queue': AdhocSyntheticDataQueueController,
+		};
+
+		const selectFieldList = {
+			'request-queue': [
+				'uuid',
+				'site_url',
+				'is_synthetic',
+				'data',
+				'logs',
+				'result',
+			],
+			'synthetic-queue': [
+				'uuid',
+				'data',
+				'logs',
+				'result',
+			],
+			'adhoc-synthetic-queue': [
+				'uuid',
+				'data',
+				'logs',
+				'result',
+			],
+		};
+
+		const queueController = queueControllerList[ params.queue ];
+		const fields = selectFieldList[ params.queue ];
+		const databaseModel = queueController.databaseModel;
+
+		/**
+		 * Prepare counts for each status.
+		 */
+		const queueHealth = await queueController.queue.checkHealth();
+
+		const statusCountsResult = await Database.select( 'status' ).count( '* as value' ).from( databaseModel.table ).groupBy( 'status' );
+		const statusCounts = {};
+		statusCountsResult.map( ( item ) => {
+			statusCounts[ item.status ] = parseInt( item.value ) || 0;
+		} );
+		queueHealth.succeeded = statusCounts.succeeded;
+
+		/**
+		 * Fetch records from respective source. (either MySQL or Redis)
+		 */
+		let items = [];
+
+		switch ( params.status ) {
+			case 'active':
+			case 'failed':
+			case 'waiting':
+			case 'succeeded':
+				const result = await Database.select( fields ).from( databaseModel.table ).where( 'status', params.status ).paginate( params.paged, params.perPage );
+				items = result.data;
+				break;
+		}
+
+		/**
+		 * Prepare fields according
+		 * @type {*[]}
+		 */
+		const prepareItemCallbacks = {
+			'request-queue': ( item ) => {
+
+				let data = item.data.toString();
+				data = Utility.maybeParseJSON( data ) || {};
+
+				const preparedItem = {
+					uuid: item.uuid,
+					site_title: data.site_info.site_title,
+					error_count: data.errorCount ? data.errorCount : 0,
+					error_sources_count: data.errorSourceCount ? data.errorSourceCount : 0,
+					urls_count: data.urls ? _.size( data.urls ) : 0,
+					is_synthetic: !! item.is_synthetic,
+				};
+
+				if ( [ 'failed', 'succeeded' ].includes( params.status ) ) {
+					preparedItem.result = item.result.toString();
+				}
+
+				return preparedItem;
+			},
+			'synthetic-queue': ( item ) => {
+				let data = item.data.toString();
+				data = Utility.maybeParseJSON( data ) || {};
+
+				const preparedItem = {
+					uuid: item.uuid,
+					domain: `${ item.data.domain }.local`,
+					plugins: Utility.parseSyntheticExtensionParam( data.plugins ),
+					theme: Utility.parseSyntheticExtensionParam( data.theme ),
+				};
+
+				if ( [ 'failed', 'succeeded' ].includes( params.status ) ) {
+					preparedItem.logs = Utility.maybeParseJSON( item.logs ) || [];
+					preparedItem.result = item.result.toString();
+				}
+
+				return preparedItem;
+			},
+			'adhoc-synthetic-queue': ( item ) => {
+				let data = item.data.toString();
+				data = Utility.maybeParseJSON( data ) || {};
+
+				const preparedItem = {
+					uuid: item.uuid,
+					domain: `${ data.domain }.local`,
+					plugins: Utility.parseSyntheticExtensionParam( data.plugins ),
+					theme: Utility.parseSyntheticExtensionParam( data.theme ),
+					amp_source: data.ampSource,
+				};
+
+				if ( [ 'failed', 'succeeded' ].includes( params.status ) ) {
+					preparedItem.logs = Utility.maybeParseJSON( item.logs ) || [];
+					preparedItem.result = item.result.toString();
+				}
+
+				preparedItem.requested_by = data.email;
+
+				return preparedItem;
+			},
+		};
+
+		const preparedItems = items.map( prepareItemCallbacks[ params.queue ] );
+
+		/**
+		 * Prepare table args.
+		 */
+		const tableArgs = {
+			tableID: 'queueTable',
+			items: preparedItems,
+			valueCallback: ( key, value ) => {
+				let htmlMarkup = '';
+
+				switch ( key ) {
+					case 'uuid':
+						value = `<abbr class="copy-to-clipboard" data-copy-text='${ value }'>${ value.slice( value.length - 13 ) }</abbr>`;
+						break;
+					case 'domain':
+						value = `<a href="${ value }" target="_blank">${ value }</a>`;
+						break;
+					case 'plugins':
+						htmlMarkup = '<ul class="list-group list-group-flush mt-0 mb-0">';
+						value = value || [];
+
+						value.map( ( item ) => {
+							htmlMarkup += `<li class="list-group-item bg-transparent">${ item.name }&nbsp;${ item.version ? item.version : '' }</li>`
+						} );
+
+						htmlMarkup += '</ul>';
+
+						value = htmlMarkup;
+						break;
+					case 'theme':
+						value = ( ! _.isEmpty( value[ 0 ] ) ) ? `${ value[ 0 ].name }&nbsp;${ value[ 0 ].version ? value[ 0 ].version : '' }` : '';
+						break;
+					case 'amp_source':
+						if ( ! [ 'wporg', 'github' ].includes( value ) ) {
+							value = `<a href="${ value }" target="_blank" title="${ value }">Other</a>`;
+						}
+						break;
+					case 'logs':
+						value = value || {};
+						htmlMarkup = '<ul class="list-group list-group-flush mt-0 mb-0">';
+
+						for ( const index in value ) {
+							const data = value[ index ];
+
+							htmlMarkup += `<li class="list-group-item bg-transparent">` +
+							              `<strong>Try ${ parseInt( index ) + 1 }</strong>:&nbsp;${ data.message }`;
+
+							if ( data.logFile ) {
+								htmlMarkup += `&nbsp;&nbsp;<a target="_blank" href="https://storage.cloud.google.com/${ data.logFile }">Log</a>`;
+							}
+
+							htmlMarkup += `</li>`;
+						}
+
+						htmlMarkup += '<ul>';
+						value = htmlMarkup;
+						break;
+					case 'data':
+					case 'result':
+						const regex = /'/gm;
+						value = value.replace( regex, '"' );
+						value = `<button class="btn btn-outline-primary btn-xs copy-to-clipboard" data-copy-text='${ value }'>Copy</button>`;
+						break;
+					case 'requested_by':
+						value = `<a href="mailto:${ value }">${ value }</a>`;
+						break;
+					case 'is_synthetic':
+						value = `<span>${ value ? 'Yes' : 'No' }</span>`;
+						break;
+
+				}
+
+				return value;
+			},
+		};
+
+		/**
+		 * Pagination.
+		 */
+		const pagination = {
+			baseUrl: `/admin/${ params.queue }/${ params.status }`,
+			total: queueHealth[ params.status ],
+			perPage: params.perPage,
+			currentPage: params.paged,
+		};
+
+		return view.render( 'dashboard/queue-secondary', {
+			queue: params.queue,
+			tabs: {
+				active: 'Active',
+				waiting: 'Waiting',
+				succeeded: 'Succeeded',
+				failed: 'Failed',
+			},
+			queueHealth: queueHealth,
+			itemCount: parseInt( queueHealth[ params.status ] ) || 0,
+			tableArgs,
+			pagination,
+		} );
 	}
 
 	/**
